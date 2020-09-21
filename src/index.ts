babff4f1f9ab5bf854d5485f47635bdf2ad3c468
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-21 22:28:56 +0530

    :sparkles: Export all functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
- 
+export { daily as spotifyDaily } from "./api/spotify";
+export { daily as rescueTimeDaily, legacy as rescueTimeLegacy } from "./api/rescuetime";
+export { daily as lastFmDaily, legacy as lastFmLegacy } from "./api/last-fm";
+export { daily as pocketCastsDaily } from "./api/pocket-casts";
+export { daily as wakatimeDaily, legacy as wakatimeLegacy } from "./api/wakatime";
+export { daily as clockifyDaily } from "./api/clockify";
+export { daily as googleFitDaily, legacy as googleFitLegacy } from "./api/google-fit";
+export { daily as ouraRingDaily, legacy as ouraRingLegacy } from "./api/oura-ring";
+export { daily as goodreadsDaily } from "./api/goodreads";
