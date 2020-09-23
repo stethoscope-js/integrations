7d36cab3162268ccf698be293734835065af3f42
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-23 20:37:50 +0530

    :white_check_mark: Add tests for class structure

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,30 @@
-test("example", () => {
-  expect("world").toBe("world");
-});
+import {
+  Spotify,
+  Rescuetime,
+  LastFm,
+  PocketCasts,
+  Wakatime,
+  Clockify,
+  GoogleFit,
+  OuraRing,
+  Goodreads,
+  Twitter,
+} from "./";
+
+[Spotify, Rescuetime, LastFm, PocketCasts, Wakatime, Clockify, GoogleFit, OuraRing, Goodreads, Twitter].forEach(
+  (ClassName) => {
+    const className = ClassName.constructor.name;
+    describe(`${className} class structure`, () => {
+      test(`${className} has an integration name`, () => {
+        const testClass = new ClassName();
+        expect(testClass.name).toBeDefined();
+        expect(typeof testClass.name).toBe("string");
+      });
+      test(`${className} has an update function`, () => {
+        const testClass = new ClassName();
+        expect(testClass.update).toBeDefined();
+        expect(typeof testClass.update).toBe("function");
+      });
+    });
+  }
+);
