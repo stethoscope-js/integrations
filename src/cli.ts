cf1125a19775f67d2936c780dcc3b99e833d85b4
Anand Chowdhary <mail@anandchowdhary.com>
2022-11-15 19:44:49 +0100

    :sparkles: Add daily full summary

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,32 @@
 #!/usr/bin/env node
+import { lstat, pathExists, readdir, readJson, writeFile } from "fs-extra";
+import { join } from "path";
 import {
-  Spotify,
-  Rescuetime,
-  LastFm,
-  PocketCasts,
-  Wakatime,
   Clockify,
-  GoogleFit,
-  OuraRing,
   Goodreads,
+  GoogleFit,
+  LastFm,
+  OuraRing,
+  PocketCasts,
+  Rescuetime,
+  Spotify,
   Twitter,
+  Wakatime,
 } from "./";
+import { zero } from "./common";
+
+const INTEGRATIONS = [
+  Clockify,
+  Goodreads,
+  GoogleFit,
+  LastFm,
+  OuraRing,
+  PocketCasts,
+  Rescuetime,
+  Spotify,
+  Twitter,
+  Wakatime,
+];
 
 const cli = async () => {
   const command = process.argv[2];
@@ -20,26 +36,53 @@ const cli = async () => {
     const start = process.argv[4];
     if (!start) throw new Error("Provide a start date");
 
-    [Spotify, Rescuetime, LastFm, PocketCasts, Wakatime, Clockify, GoogleFit, OuraRing, Goodreads, Twitter].forEach(
-      (ClassName) => {
-        const integrationObject = new ClassName();
-        if (integration === integrationObject.name) {
-          integrationObject.legacy(start);
-        }
+    INTEGRATIONS.forEach((ClassName) => {
+      const integrationObject = new ClassName();
+      if (integration === integrationObject.name) {
+        integrationObject.legacy(start);
       }
-    );
+    });
   } else if (command === "summary") {
     const integration = process.argv[3];
     if (!integration) throw new Error("Provide an integration");
 
-    [Spotify, Rescuetime, LastFm, PocketCasts, Wakatime, Clockify, GoogleFit, OuraRing, Goodreads, Twitter].forEach(
-      (ClassName) => {
-        const integrationObject = new ClassName();
-        if (integration === integrationObject.name) {
-          integrationObject.summary();
+    INTEGRATIONS.forEach(async (ClassName) => {
+      const integrationObject = new ClassName();
+      if (integration === integrationObject.name) {
+        integrationObject.summary();
+      }
+    });
+
+    const createdIntegrationData = await readdir(join(".", "data"));
+    for (const dir of createdIntegrationData) {
+      const summary: Record<string, any> = {};
+      if (
+        (await pathExists(join(".", "data", dir, "summary", "days"))) &&
+        (await lstat(join(".", "data", dir, "summary", "days"))).isDirectory()
+      ) {
+        const years = await readdir(join(".", "data", dir, "summary", "days"));
+        for (const year of years) {
+          if (
+            (await pathExists(join(".", "data", dir, "summary", "days", year))) &&
+            (await lstat(join(".", "data", dir, "summary", "days", year))).isDirectory()
+          ) {
+            const months = await readdir(join(".", "data", dir, "summary", "days", year));
+            for (const month of months) {
+              const file = join(".", "data", dir, "summary", "days", year, month);
+              const data = (await readJson(file)) as Record<string, any>;
+              Object.values(data).forEach(([day, value]) => {
+                summary[`${zero(year)}-${zero(month)}-${zero(day)}`] = value;
+              });
+            }
+          }
         }
       }
-    );
+      if (Object.keys(summary).length)
+        await writeFile(
+          join(".", "data", dir, "summary", "days.json"),
+          JSON.stringify(summary, null, 2) + "\n"
+        );
+    }
   } else {
     throw new Error(`CLI command '${command}' not recognized`);
   }
